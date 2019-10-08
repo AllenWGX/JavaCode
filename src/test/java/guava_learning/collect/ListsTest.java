403e8ba6d3af174fb2d99b9da5caf8bbe3dd9044
1045213509@qq.com <1045213509@qq.com>
2019-10-08 22:32:32 +0800

    Update ListsTest

--- src/test/java/guava_learning/collect/ListsTest.java
+++ src/test/java/guava_learning/collect/ListsTest.java
@@ -24,7 +24,28 @@ public class ListsTest {
 		try {
 			intLst.add(4);
 		}catch(UnsupportedOperationException ex) {
+			ex.printStackTrace();
 			System.out.println(intLst);
 		}
+	}
+	
+	@Test
+	public void cartesianProductTest() {
+		List<String> userLst = Lists.newArrayList("user1","user2");
+		List<String> itemLst = Lists.newArrayList("item1","item2");
+		List<List<String>> result = Lists.cartesianProduct(userLst, itemLst);
+		System.out.println(result);
+	}
+	
+	@Test
+	public void stringToCharLstTest() {
+		List<Character> charLst = Lists.charactersOf("Hello World");
+		System.out.println(charLst);
+	}
+	
+	@Test
+	public void partitionTest() {
+		List<List<Integer>> result = Lists.partition(Lists.newArrayList(1,2,3,4), 2);
+		System.out.println("After Partiton: " + result);
 	}
 }
