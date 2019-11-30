49ce70b2e0accae2516e42dc00f30b5a3a0da436
1045213509@qq.com <1045213509@qq.com>
2019-11-30 22:03:26 +0800

    Update BiMap Test, Add assert

--- src/test/java/guava_learning/collect/BiMapTest.java
+++ src/test/java/guava_learning/collect/BiMapTest.java
@@ -1,6 +1,8 @@
 package guava_learning.collect;
 
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
@@ -11,7 +13,8 @@ public class BiMapTest {
 		BiMap<String,Integer> bimap = HashBiMap.create();
 		bimap.put("samsung", 1);
 		bimap.put("vivo", 2);
-		System.out.println(bimap);
+		//System.out.println(bimap);
+		assertThat(bimap.toString(),is("{samsung=1, vivo=2}"));
 		//
 		BiMap<Integer,String> bimap_inverse = bimap.inverse();
 		System.out.println(bimap_inverse);
@@ -25,9 +28,10 @@ public class BiMapTest {
 		try {
 			bimap.put("vivo", i);
 		}catch(IllegalArgumentException ex) {
-			ex.printStackTrace();
+			assertThat(bimap.toString(), is("{samsung=1}"));
 		}
 		bimap.forcePut("vivo", i);
-		System.out.println(bimap);
+		//System.out.println(bimap);
+		assertThat(bimap.toString(), is("{vivo=1}"));
 	}
 }
